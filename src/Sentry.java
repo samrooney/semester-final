1dc148e61d168536c25c6f7ba2b556bb07163361
samrooney <sam_rooney@asl.org>
2014-12-16 09:48:19 +0000

    csbdkhph

--- src/Sentry.java
+++ src/Sentry.java
@@ -1,10 +1,21 @@
 import org.asl.karelx.Uberbot;
+import edu.fcps.karel2.Display;
 
 /**
  * Patrols an area defined by four beepers.
  * 
  */
 public class Sentry extends Uberbot {
+
+   public Sentry()
+  {
+     super(1, 1, Display.NORTH, Display.INFINITY);
+  }
+   public Sentry(int x, int y, int dir, int beep)
+  {
+     super(x, y, dir, beep);
+  }
+
 
 	// Provide one or more constructors, as you wish
 	
--- src/TestChallenge1.java
+++ src/TestChallenge1.java
@@ -1,15 +1,89 @@
-/**
- * Read and implement all of the TODO items in the main method, below.
- * 
- */
-public class TestChallenge1 {
-	// TODO Complete the provided Sentry class by implementing its patrol() method
 
-	public static void main(String[] args) {
+
+   // TODO Complete the provided Sentry class by implementing its patrol() method
+
+	
 		// TODO Load a default map 
 		// TODO Create a Squarebot and have it place beepers at the corners of a rectangle
 		//   	with bottom right corner at (9, 3) and sized for 5 beepers on each side (see picture)
 		// TODO Create a Sentry and have it patrol clockwise around the beepers, indefinitely
-	}
 
+
+
+
+import edu.fcps.karel2.Display;
+import org.asl.karelx.Squarebot;
+
+public class TestChallenge1 {
+public static void patrol(Sentry arg)
+
+{
+
+while (arg.nextToABeeper())
+      {
+       Dooby.turnRight();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
 }
+
+public static void main(String[] args) {
+		 
+       
+		 Display.openWorld("maps/default.map");
+		 Display.setSize(10, 10);
+		 Display.setSpeed(10);
+       
+       Sentry Dooby = new Sentry(9, 3, Display.WEST, 5);
+		 
+       Dooby.putBeeper();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       Dooby.putBeeper();
+       Dooby.turnRight();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       Dooby.putBeeper();
+       Dooby.turnRight();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       Dooby.putBeeper();
+       Dooby.turnRight();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       Dooby.move();
+       patrol(Dooby);
+       
+ }
+ 
+ }
+ 
+ }
+       
+
+       
+       
+   
+  
+
+       
+
+       
+       
+       
+       
+   
+      
